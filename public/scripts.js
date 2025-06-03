ee47ef3d0605942db163c7d5d1f7022e37a3d7e8
ValeriaDeCasas19 <valeriadecasasa@gmail.com>
2025-06-03 13:01:39 -0600

    estilos y resumen de vehiculo

--- public/scripts.js
+++ public/scripts.js
@@ -14,20 +14,21 @@ if (formEntrada) {
 
     const data = await res.json();
     if (data.mensaje) {
-      alert("✅ " + data.mensaje);
+      alert("Bien" + data.mensaje);
     } else {
-      alert("❌ " + (data.error || "Error al registrar"));
+      alert("X" + (data.error || "Error al registrar"));
     }
   });
 }
 
 
 const formSalida = document.getElementById('formSalida');
+
 if (formSalida) {
   formSalida.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const placas = document.getElementById('placas').value;
+    const placas = document.getElementById('placas').value.trim();
 
     const res = await fetch("/api/vehiculos/salida", {
       method: "POST",
@@ -38,9 +39,33 @@ if (formSalida) {
     const data = await res.json();
 
     if (data.mensaje) {
-      alert("✅ " + data.mensaje);
+      //convierte minutos a horas
+      const tiempo = data.minutos >= 60
+        ? `${Math.floor(data.minutos / 60)} h ${data.minutos % 60} min`
+        : `${data.minutos} min`;
+
+      //da formato al pago
+      const pago = `$${parseFloat(data.pago).toFixed(2)}`;
+
+      document.getElementById('resultado').innerHTML = `
+        <h3 style="margin-top:20px;">Resumen de salida</h3>
+        <table style="margin:auto; border-collapse:collapse;">
+          <tr>
+            <th style="padding:6px;">Núm. Placa</th>
+            <th style="padding:6px;">Tiempo Estacionado</th>
+            <th style="padding:6px;">Tipo</th>
+            <th style="padding:6px;">Cantidad a Pagar</th>
+          </tr>
+          <tr style="text-align:center;">
+            <td style="padding:6px;">${data.placas}</td>
+            <td style="padding:6px;">${tiempo}</td>
+            <td style="padding:6px;">${data.tipo}</td>
+            <td style="padding:6px;">${pago}</td>
+          </tr>
+        </table>
+      `;
     } else {
-      alert("❌ " + (data.error || "Error al registrar"));
+      document.getElementById('resultado').innerHTML = `<p style="color:red;">❌ ${data.error || 'Error al registrar'}</p>`;
     }
   });
 }
